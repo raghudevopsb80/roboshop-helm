9d1f998a8a4619b9ed791fdc8c8c2236297b9697
SVR Kalluru <raghuk.vit@gmail.com>
2024-09-27 06:03:10 +0530

    Roboshop Code

--- all.sh
+++ all.sh
@@ -1,6 +1,6 @@
 bash helm.sh dev catalogue 1.0.1
-bash helm.sh dev user 1.0.1
-bash helm.sh dev cart 1.0.0
-bash helm.sh dev shipping 1.0.0
+bash helm.sh dev user 1.0.2
+bash helm.sh dev cart 1.0.1
+bash helm.sh dev shipping 1.0.1
 bash helm.sh dev payment 1.0.2
-bash helm.sh dev frontend 1.0.2
+bash helm.sh dev frontend 1.0.6
